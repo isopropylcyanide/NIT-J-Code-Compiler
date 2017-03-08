033f1cf890d363c5851cbcdb260a18a7532cb5df
Aman Garg <amangargcse@outlook.com>
2017-03-09 00:25:23 +0530

    Jquery call working, data being passed easily

--- editor/urls.py
+++ editor/urls.py
@@ -3,5 +3,5 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.index, name='index'),
-    url(r'^execute$', views.execute, name='execute')
+    url(r'^execute$', views.execute, name='index')
 ]
--- editor/views.py
+++ editor/views.py
@@ -1,15 +1,20 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.views.decorators.csrf import csrf_exempt
 
 
 def index(request):
     return render(request, 'editor/home.html')
 
 
+@csrf_exempt
 def execute(request):
     """Create a file on server code.language
         compile it using the script provided
         and return the result
     """
-    print 'I reached here 2'
-    return render(request, 'editor/home.html')
+    if request.is_ajax():
+        source_code = request.POST.get('sourceCode', '')
+        source_lang = request.POST.get('sourceLang', '')
+        print source_code, source_lang
+        return HttpResponse("OK")
