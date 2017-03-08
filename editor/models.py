7a246d3f30542766d22f30d848e9aeb3f25c6848
Aman Garg <amangargcse@outlook.com>
2017-03-08 23:42:47 +0530

    Installed jQuery and created a text editor model

--- editor/models.py
+++ editor/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Text_Editor(models.Model):
+    # A text editor stores source code per language for saved sessions
+    text = models.CharField(max_length=2000)
+    lang = models.CharField(max_length=20)
--- editor/views.py
+++ editor/views.py
@@ -4,3 +4,12 @@ from django.http import HttpResponse
 
 def index(request):
     return render(request, 'editor/home.html')
+
+
+def execute(request):
+    """Create a file on server code.language
+        compile it using the script provided
+        and return the result
+    """
+    print 'I reached here 2'
+    return render(request, 'editor/home.html')
