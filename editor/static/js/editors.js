bc74b78f057d4e5a83675162a988a5f25be034a5
Aman Garg <amangargcse@outlook.com>
2017-03-29 19:22:04 +0530

    multitab almost done

--- editor/static/js/editors.js
+++ editor/static/js/editors.js
@@ -5,6 +5,8 @@ editorMap = {
 };
 
 activeIndex = undefined;
+uniqueID = 0;
+var tabs = $('#tabs');
 
 class allEditors {
     // constructor containing
@@ -17,9 +19,9 @@ class allEditors {
         //for code properties
         return editorMap[activeIndex];
     }
-    setActiveEditor(activeIndex) {
+    setActiveEditor(activeInd) {
         //when a tab receives focus
-        activeIndex = activeIndex;
+        activeIndex = activeInd;
     }
     removeEditor(removedIndex) {
         //when delete is called
@@ -34,18 +36,17 @@ var editorList = new allEditors();
 
 //add tab button is clicked
 $("#add-tab").click(function() {
-    var num_tabs = $("#tabs ul li").length + 1;
     $("#tabs ul").append(
-        "<li><a href='#tab" + num_tabs + "'> New Tab </a><span class=\"ui-icon ui-icon-close\"</li>"
+        "<li><a href='#tab" + uniqueID + "'> New Tab"+(uniqueID)+" </a><span class=\"ui-icon ui-icon-close\"</li>"
     );
     $("#tabs").append(
-        "<div id='tab" + num_tabs + "'>" +
+        "<div id='tab" + uniqueID + "'>" +
         "<div class=\"form-group\" class=\"text-editor-panel\">" +
-        "<textarea class=\"form-control\" id=\"codeEditor" + num_tabs + "\">//Your text goes here</textarea>" +
+        "<textarea class=\"form-control\" id=\"codeEditor" + uniqueID + "\">//Your text goes here</textarea>" +
         "</div></div>"
     );
     //once the base textarea has been added
-    var localEditor = CodeMirror.fromTextArea(document.getElementById("codeEditor" + num_tabs), {
+    var localEditor = CodeMirror.fromTextArea(document.getElementById("codeEditor" + uniqueID), {
         // Create codemirror instance
         lineNumbers: true,
         lineWrapping: true,
@@ -53,15 +54,30 @@ $("#add-tab").click(function() {
         mode: "text/x-c++src",
         styleActiveLine: true
     });
-    editorList.addEditor(num_tabs, localEditor);
+    editorList.addEditor(uniqueID, localEditor);
     $('#languageSelect').trigger("change");
     selectTheme();
     $("#tabs").tabs("refresh");
+    //select the tab on load
+    $("#tabs").tabs("option", "active", uniqueID);
+    uniqueID++;
 });
 
 // Close icon: removing the tab on click
-$('#tabs').on("click", "span.ui-icon-close", function() {
+tabs.on("click", "span.ui-icon-close", function() {
     var panelId = $(this).closest("li").remove().attr("aria-controls");
+    var indexRemoved = panelId.replace('tab', '');
+    editorList.removeEditor(indexRemoved);
+    displayOutput('remove: ' + indexRemoved + ' now: ' + editorList.toString() + ' uid: ' + uniqueID+' currentActive: ' + activeIndex);
     $("#" + panelId).remove();
     tabs.tabs("refresh");
+});
+
+tabs.tabs({
+    activate: function(event, ui) {
+        var panelId = ui.newPanel[0].id;
+        var curIndex = panelId.replace('tab', '');
+        editorList.setActiveEditor(curIndex);
+        displayOutput('act: ' + curIndex + ' now: ' + editorList.toString() + ' uid: ' + uniqueID+' currentActive: ' + activeIndex);
+    }
 });
